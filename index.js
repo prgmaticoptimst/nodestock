25593fee5e16f8a0e26eb9931254fc2d0fe1916d
Brooks Balogh <brooksbalogh@gmail.com>
2023-01-10 14:27:20 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const exphbs = require('express-handlebars');
+const path = require('path');
+
+const PORT = process.env.PORT || 5000;
+
+//Set Handlebars Middleware
+app.engine('handlebars', exphbs.engine());
+app.set('view engine', 'handlebars');
+
+// Set Handlebar Routes
+app.get('/', (req, res) => {
+    res.render('home', {
+        stuff: "This is stuff..."
+    });
+});
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.listen(PORT, () => console.log('Server Listening on port ' + PORT))
